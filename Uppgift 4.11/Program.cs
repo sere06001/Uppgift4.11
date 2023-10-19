c6e5be2341a6b0db677cf19e261e69f81efa41e0
sere06001 <sere06001@utb.vaxjo.se>
2023-10-19 19:37:54 +0200

    Add project files.

---
+++ Uppgift 4.11/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+namespace Uppgift4_11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv ett ord");
+            string ord = Console.ReadLine();
+            var lista = new List<char>();
+            foreach (var item in ord) 
+            {
+                int charCount = 0;
+                foreach (var chars in ord)
+                {
+                    if (item == chars)
+                    {
+                        charCount++;
+                    }
+                }
+
+                if (charCount > 1 && !lista.Contains(item))
+                {
+                    lista.Add(item);
+                    Console.WriteLine($"'{item}' förekommer mer än 1 gång.");
+                }
+            }
+            bool tomLista = !lista.Any();
+            if (tomLista)
+            {
+                Console.WriteLine("Inget tecken förekommer mer än 1 gång");
+            }
+            Console.ReadKey();
+        }
+    }
+}
